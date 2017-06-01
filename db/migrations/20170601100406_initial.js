f4eadd2e799715602a54e68f15fedc5032457161
Franklin Crosby <obleo33@gmail.com>
2017-06-01 10:18:38 -0600

    Add first database migration to create table

--- db/migrations/20170601100406_initial.js
+++ db/migrations/20170601100406_initial.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('garage', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('reason');
+      table.string('cleanliness');
+      table.timestamps(true, true);
+    }),
+  ]);
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('garage'),
+  ]);
 };
