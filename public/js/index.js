3ba7eba78f2ba98689d3275cbba0b6ce85f626ae
Franklin Crosby <obleo33@gmail.com>
2017-06-01 16:31:26 -0600

    Add add form and fetch

--- public/js/index.js
+++ public/js/index.js
@@ -20,37 +20,60 @@ const addItem = (name, reason, cleanliness) =>  {
     body: JSON.stringify({ name, reason, cleanliness}),
   })
     .then(response => response.json())
-    .then(link => appendItem());
+    .then(item => {
+      itemsArr.push(item)
+      showAllItems(itemsArr)
+    })
 }
 
 const showAllItems = (items) => {
   console.log(itemsArr);
   $("#garage-inventory").empty()
+  garageNumbers()
   items.map(item => {
-    appendItem(item);
+    $('#garage-inventory').append(`
+      <div class="item-card">
+        <h2 id="${item.id}" name="${item.id}" class="item-name">${item.name}</h2>
+      </div>
+    `)
   })
 }
 
-const appendItem = (item) => {
-  $('#garage-inventory').append(`
-    <div class="item-card">
-      <h2 id="${item.id}" name="${item.id}" class="item-name">${item.name}</h2>
-    </div>
-  `)
+const garageNumbers = () => {
+  let nubers = itemsArr.reduce((numbers, item) => {
+    
+  })
 }
 
 $('#garage-door').click(() => {
   doorOpen = !doorOpen
-
   if (doorOpen) {
     $('#garage-door').text('Close Garage Door')
     showAllItems(itemsArr)
+    $('.garage').toggleClass('garage-open')
   } else {
     $('#garage-door').text('Open Garage Door')
+    $('.garage').toggleClass('garage-open')
   }
 });
 
-$('#add-new-item').click((e) => {
+$(document).keyup(() => {
+  const isName = $('#name-input').val().length
+  const isReason = $('#reason-input').val().length
+  if(isName && isReason ){
+    $('#add-new-item').prop('disabled', false)
+  } else {
+    $('#add-new-item').prop('disabled', true)
+  }
+})
+
+$('#add-new-item').click( (e) => {
   e.preventDefault()
-  console.log('here');
+  let name = $('#name-input').val()
+  let reason = $('#reason-input').val()
+  let cleanliness = $('#cleanliness-select').val()
+  $('#name-input').val('')
+  $('#reason-input').val('')
+  $('#add-new-item').prop('disabled', true)
+  addItem(name, reason, cleanliness)
 })
